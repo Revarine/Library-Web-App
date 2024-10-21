b236a2f6e76fd11ac8c00243b3b87d58c68007d1
Revarine <sashasakovich9@gmail.com>
2024-10-21 10:09:26 +0300

    Migrations

--- src/Library.Infrastructure/Migrations/20241021070913_UpdateAuthor.Designer.cs
+++ src/Library.Infrastructure/Migrations/20241021070913_UpdateAuthor.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Library.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Library.Infrastructure.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241021070913_UpdateAuthor")]
+    partial class UpdateAuthor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
---
+++ src/Library.Infrastructure/Migrations/20241021070913_UpdateAuthor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Genres",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DateOfBirth",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Authors");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Id",
+                table: "Genres",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
